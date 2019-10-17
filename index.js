4eec8d121d8b3c5020ff1153b345a4f4d7167ab6
Matthew Kay <mkay0961@gmail.com>
2019-10-17 15:18:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,34 +10,29 @@ function destructivelyAppendKitten(name){
 }
 
 function destructivelyPrependKitten(name){
-  kittens.unshift(name);
-  return kittens;
+
 }
 
 function destructivelyRemoveLastKitten(){
-  kittens.pop();
-  return kittens;
+
 }
 
 function destructivelyRemoveFirstKitten(){
-  kittens.shift();
-  return kittens;
+  
 }
 
 function appendKitten(name){
-  return [...kittens, name]
+
 }
 
 function prependKitten(name){
-  return [name, ...kittens]
+
 }
 
 function removeLastKitten(){
-  var newArray = kittens.slice(0, kittens.length - 1);
-  return newArray;
+
 }
 
 function removeFirstKitten(){
-  var newArray = kittens.slice(1);
-  return newArray;
+
 }
